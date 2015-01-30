7dd4b7dfb3cabf67832489ce0b02f2d5df704874
Gennadi Oumanski <go@localz.co>
2015-01-30 17:09:57 +1100

    Version 2 of SDK, including integrations and geofence support

--- README.md
+++ README.md
@@ -8,10 +8,15 @@ The Spotz Android SDK allows your Android app to detect when it is in range of y
 Changelog
 =========
 
-**1.3.1**
+**2.0.2**	
+* Added geofence support.	
+* Added monitoring of subset of spotz.	
+* Added integration with 3rd party systems support.
+
+**1.3.1**	
 * Fixed triggering on Spotz that did not exist for the application.
 
-**1.3.0**
+**1.3.0**	
 * Initial public release.
 
 What does the sample app do?
@@ -34,11 +39,11 @@ The sample app requires devices running Android 4.3 or newer.
     
     If you're using **Android Studio**, simply 'Open' the project.
 
-    If you're using **Eclipse ADT**, in your workspace do File -> Import -> General -> Existing Projects into Workspace.
+    If you're using **Eclipse ADT**, in your workspace do File -> Import -> General -> Existing Projects into Workspace first for google-play-services-lib library project and then for the main project.
     
     *The project targets Android 4.4 (API level 19) so check you have this version in your Android SDK.*
     
-  3. Define a Spot using the [Spotz console](http://spotz.localz.co). Don't forget to add a beacon to your Spot. If you don't have a real beacon, don't worry, you can use the Beacon Toolkit app:
+  3. Define a Spot using the [Spotz console](http://spotz.localz.com). Don't forget to add a beacon to your Spot. If you don't have a real beacon, don't worry, you can use the Beacon Toolkit app:
   
     <a href="https://itunes.apple.com/us/app/beacon-toolkit/id838735159?ls=1&mt=8">
     <img alt="Beacon Toolkit on App Store" width="100" height="33"
@@ -61,7 +66,8 @@ The sample app requires devices running Android 4.3 or newer.
 How to add the SDK to your own Project
 ======================================
 
-Your project must support minimum Android 2.3.3 API level 10. 
+Your project must support minimum Android 2.3.3 API level 10.	
+Ensure that using ["Android SDK Manager"](http://developer.android.com/tools/help/sdk-manager.html) you downloaded "Google Play Services" Rev.22 or later. 
 
 If you're a **Gradle** user you can easily include the library by specifying it as
 a dependency in your build.gradle script:
@@ -74,9 +80,10 @@ a dependency in your build.gradle script:
     }
     ...
     dependencies {
-        compile 'com.localz.spotz.sdk:spotz-sdk-android:1.3.1@aar'
-        compile 'com.localz.spotz.sdk:spotz-sdk-api:1.1.0'
-        compile 'com.localz.proximity.ble:ble-sdk-android:1.1.1@aar'
+        compile 'com.localz.spotz.sdk:spotz-sdk-android:2.0.2@aar'
+        compile 'com.localz.spotz.sdk:spotz-sdk-api:1.2.2'
+        compile 'com.localz.proximity.ble:ble-sdk-android:1.1.2@aar'
+        compile 'com.google.android.gms:play-services:6.5.+'
         ...
     }
 
@@ -86,20 +93,20 @@ If you're a **Maven** user you can include the library in your pom.xml:
     <dependency>
       <groupId>com.localz.spotz.sdk</groupId>
       <artifactId>spotz-sdk-android</artifactId>
-      <version>1.3.1</version>
+      <version>2.0.2</version>
       <type>aar</type>
     </dependency>
     
     <dependency>
       <groupId>com.localz.spotz.sdk</groupId>
       <artifactId>spotz-sdk-api</artifactId>
-      <version>1.1.0</version>
+      <version>1.2.2</version>
     </dependency>
     
     <dependency>
       <groupId>com.localz.proximity.ble</groupId>
       <artifactId>ble-sdk-android</artifactId>
-      <version>1.1.1</version>
+      <version>1.1.2</version>
       <type>aar</type>
     </dependency>
 
@@ -114,15 +121,19 @@ If you're a **Maven** user you can include the library in your pom.xml:
         ...
     </repositories>
     ...
+    
+You will also need add dependency to google play services. Google play services is not available via public maven repositories. You will need to create package (apklib or aar), load to your local maven repository and then use it as reference in your pom.xml. The following tool should help: [https://github.com/simpligility/maven-android-sdk-deployer/](https://github.com/simpligility/maven-android-sdk-deployer/). 
 
 Otherwise, if you are old school, you can manually copy all the JARs in the libs folder and add them to your project's dependencies. Your libs folder will have at least the following JARs:
 
-- ble-sdk-android-1.1.1.jar
+- ble-sdk-android-1.1.2.jar
 - google-http-client-1.19.0.jar
 - google-http-client-gson-1.19.0.jar
 - gson-2.3.jar
-- spotz-sdk-android-1.3.1.jar
-- spotz-sdk-api-1.1.0.jar
+- spotz-sdk-android-2.0.2.jar
+- spotz-sdk-api-1.2.2.jar
+
+and also add "google play services lib" library project to your project. For instructions refer to [http://developer.android.com/google/play-services/setup.html](http://developer.android.com/google/play-services/setup.html). Select "Using Eclipse with ADT". 
 
 How to use the SDK
 ==================
@@ -140,16 +151,21 @@ There are only 3 actions to implement - **initialize, scan, and listen!**
         <uses-permission android:name="android.permission.INTERNET" />
         <uses-permission android:name="android.permission.BLUETOOTH" />
         <uses-permission android:name="android.permission.BLUETOOTH_ADMIN" />
+        <uses-permission android:name="android.permission.ACCESS_FINE_LOCATION"/> 
+        <uses-permission android:name="android.permission.ACCESS_COARSE_LOCATION"/>
 
   2. Define the following service in your AndroidManifest.xml:
 
         <service android:name="com.localz.proximity.ble.services.BleHeartbeat" />
+        <service android:name="com.localz.spotz.sdk.geofence.GeofenceTransitionsIntentService"/>
         
   3. Initialize the SDK by providing your application ID and client key (as shown on Spotz console):
   
         Spotz.getInstance().initialize(context, // Your context
                 "your-application-id",          // Your application ID goes here
                 "your-client-key",              // Your client key goes here
+                null,
+                null,
                 new InitializationListenerAdapter() {
                     @Override
                     public void onInitialized() {
@@ -262,6 +278,93 @@ Your project is now ready to start using the Spotz SDK!
                 <action android:name="com.foo.app.SPOTZ_ON_SPOT_EXIT" />
             </intent-filter>
         </receiver>
+ 
+ #### On 3rd party Integration response received
+
+  To listen for when response received from third party integration systems, define a <code>BroadcastReceiver</code> that filters for action <code>\<your package\>.SPOTZ_ON_INTEGRATION_RESPONDED</code> e.g. <code>com.foo.myapp.SPOTZ_ON_INTEGRATION_RESPONDED</code>
+    You can find your package name in AndroidManifest.xml file:
+
+        <manifest xmlns:android="http://schemas.android.com/apk/res/android"
+                package="com.foo.myapp" >
+        
+  Here's an example (don't forget to unregister your BroadcastReceiver when no longer needed):
+
+        BroadcastReceiver enteredSpotBroadcastReceiver = new OnEnteredSpotBroadcastReceiver();
+        registerReceiver(enteredSpotBroadcastReceiver,
+                new IntentFilter(getPackageName() + ".SPOTZ_ON_INTEGRATION_RESPONDED"));
+
+        ...
+
+        public class OnIntegrationRespondedBroadcastReceiver extends BroadcastReceiver {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                String integrationResponse = (String) intent.getSerializableExtra(Spotz.EXTRA_INTEGRATION);
+                
+                // Do something with the response from 3rd party systems!
+                // As response specific to the Response is specific to the 
+            }
+        }
+
+  Or if you prefer, you can define your BroadcastReceiver in AndroidManifest.xml:
+
+        <receiver android:name="com.foo.app.services.OnIntegrationRespondedBroadcastReceiver" >
+            <intent-filter>
+                <action android:name="com.foo.app.SPOTZ_ON_INTEGRATION_RESPONDED" />
+            </intent-filter>
+        </receiver>
+        
+
+
+Advanced Features
+=================
+#### Monitoring subset of spotz
+
+You might want to monitor not all Spotz but subset of spotz in your application. In this case, on [Spotz console](http://spotz.localz.com) for the spotz that you want to monitor, you can set an attribute (or few attributes) with the value. Later when initialising Spotz android SDK, you can pass attribute(s) name and value(s) to only monitor for the matching spotz. 
+In this case, SDK initialization will be similar to the following:
+<pre>
+        <b>Map<String, String> attributes = new HashMap<String, String>();
+        attributes.put("show", "yes"); 
+        attributes.put("city", "Melbourne");</b>     
+        Spotz.getInstance().initialize(context, // Your context
+                "your-application-id",          // Your application ID goes here
+                "your-client-key",              // Your client key goes here
+                <b>attributes,</b>
+                null,
+                new InitializationListenerAdapter() {
+                    @Override
+                    public void onInitialized() {
+                        // Now that we're initialized, we can start scanning for Spotz here 
+                    }
+                }
+        );
+</pre>
+
+#### Integration with 3rd party systems
+
+[Spotz integration guide] (http://spotz.localz.com) introduces the concept and provides details of how to add integration to spotz. Sometimes you might want to provide indentity of the user that uses your application to the system that you integrate with. This is achieve by provide the identity attributes to Spotz when initialising Spotz SDK. E.g.:
+<pre>
+        <b>final DeviceUpdateIdsPutRequest updateIdsRequest = new DeviceUpdateIdsPutRequest();
+		updateIdsRequest.ids = new DeviceUpdateIdsPutRequest.Ids();
+		updateIdsRequest.ids.payload = new HashMap<String, String>();
+		updateIdsRequest.ids.payload.put("customerAccount", "user123");
+		// the statement above will make customerAccount value "user123" 
+		// available to all 3rd party integration systems. 
+		// Should you wish to pass the value ONLY to a one 3rd party system, 
+		// the syntax is "integrationName.idName", e.g.
+		updateIdsRequest.ids.payload.put("zapierWebhook.privateUserId", "#565589"); </b>     
+        Spotz.getInstance().initialize(context, // Your context
+                "your-application-id",          // Your application ID goes here
+                "your-client-key",              // Your client key goes here
+                null,
+                <b>updateIdsRequest</b>,
+                new InitializationListenerAdapter() {
+                    @Override
+                    public void onInitialized() {
+                        // Now that we're initialized, we can start scanning for Spotz here 
+                    }
+                }
+        );
+</pre>
 
 Contribution
 ============
@@ -271,6 +374,6 @@ For bugs, feature requests, or other questions, [file an issue](https://github.c
 License
 =======
 
-Copyright 2014 [Localz Pty Ltd](http://www.localz.co/)
+Copyright 2015 [Localz Pty Ltd](http://www.localz.com/)
 
  
--- src/com/localz/spotz/sdk/app/MainActivity.java
+++ src/com/localz/spotz/sdk/app/MainActivity.java
@@ -12,14 +12,21 @@ import android.content.pm.PackageManager;
 import android.graphics.drawable.TransitionDrawable;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.localz.spotz.sdk.app.widgets.CustomAnimation;
 import com.localz.spotz.sdk.Spotz;
+import com.localz.spotz.sdk.app.widgets.CustomAnimation;
 import com.localz.spotz.sdk.listeners.InitializationListenerAdapter;
 import com.localz.spotz.sdk.models.Spot;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Iterator;
 
 public class MainActivity extends Activity {
     public static final String TAG = MainActivity.class.getSimpleName();
@@ -27,7 +34,9 @@ public class MainActivity extends Activity {
 
     private OnEnteredSpotBroadcastReceiver enteredSpotBroadcastReceiver;
     private OnExitedSpotBroadcastReceiver exitedSpotBroadcastReceiver;
-    private boolean isInRange = false;
+    private OnIntegrationRespondedBroadcastReceiver integrationRespondedBroadcastReceiver;
+    // Tracks spot ids of the spots that device is in
+    HashMap<String, Spot> inSpotMap = new HashMap<String, Spot>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,10 +46,12 @@ public class MainActivity extends Activity {
         // Register BLE scan receivers
         enteredSpotBroadcastReceiver = new OnEnteredSpotBroadcastReceiver();
         exitedSpotBroadcastReceiver = new OnExitedSpotBroadcastReceiver();
+        integrationRespondedBroadcastReceiver = new OnIntegrationRespondedBroadcastReceiver();
         registerReceiver(enteredSpotBroadcastReceiver,
                 new IntentFilter(getPackageName() + ".SPOTZ_ON_SPOT_ENTER"));
         registerReceiver(exitedSpotBroadcastReceiver,
                 new IntentFilter(getPackageName() + ".SPOTZ_ON_SPOT_EXIT"));
+        registerReceiver(integrationRespondedBroadcastReceiver, new IntentFilter(getPackageName() + ".SPOTZ_ON_INTEGRATION_RESPONDED"));
 
         // Check if device has ble
         if (getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
@@ -65,17 +76,16 @@ public class MainActivity extends Activity {
         Spotz.getInstance().initialize(this,
                 "your-application-id", // Your application ID goes here
                 "your-client-key", // Your client key goes here
-
+                null,
+                null,
                 new InitializationListenerAdapter() {
                     @Override
                     public void onInitialized() {
-                        // Start scanning for spotz now that we're initialized
-                        Spotz.getInstance().startScanningForSpotz(MainActivity.this, Spotz.ScanMode.EAGER);
 
                         TextView rangeText = (TextView) findViewById(R.id.activity_range_text);
 
                         if (getString(R.string.message_initializing).equalsIgnoreCase(rangeText.getText().toString())) {
-                            setOutOfRange();
+                            setOutOfRange(null);
                         }
 
                         CustomAnimation.startWaveAnimation(findViewById(R.id.wave));
@@ -83,7 +93,7 @@ public class MainActivity extends Activity {
 
                     @Override
                     public void onError(Exception exception) {
-                        Log.e(TAG, "Exception while registering device ");
+                        Log.e(TAG, "Exception while registering device", exception);
 
                         runOnUiThread(new Runnable() {
                             @Override
@@ -139,13 +149,128 @@ public class MainActivity extends Activity {
     public class OnExitedSpotBroadcastReceiver extends BroadcastReceiver {
         @Override
         public void onReceive(Context context, Intent intent) {
-            setOutOfRange();
+            Spot spot = (Spot) intent.getSerializableExtra(Spotz.EXTRA_SPOTZ);
+            setOutOfRange(spot);
         }
+    }
+
+    /**
+     * Optional!!!!!!!!. Only used if there is an integration configured for the application which require response.
+     */
+    public class OnIntegrationRespondedBroadcastReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String integrationResponse = (String) intent.getSerializableExtra(Spotz.EXTRA_INTEGRATION);
+
+            Log.d(TAG, "Integration Response: " + integrationResponse);
+
+            /*
+               The response will have the following format:
+               {
+                 "date": "2015-01-07T20:34:00.594Z",
+                 "548662128f658a08006d684d": {
+                 "httpGetWebhook": {
+                        "base": "USD",
+                        "date": "2015-01-07",
+                        "rates": {
+                           "AUD": 1.2391,
+                           "EUR": 0.8452
+                        }
+                 }
+               }
+
+               where data - is the timestamp when actual response occurred
+               548662128f658a08006d684d - is spot id
+               httpGetWebhook - is the name of the integration, as shown in the Spotz Web Dashboard.
+               The value of "httpGetWebhook" is the JSON string of the response from the integration.
+               extractInfoFromIntegrationResponse - is method specific to parsing response from the
+               3rd party API.
+             */
+
+            try {
+                JSONObject rootObject = new JSONObject(integrationResponse);
+                Iterator keys = rootObject.keys();
+                if (keys != null) {
+                    while (keys.hasNext()) {
+                        String key = (String) keys.next();
+                        if (key.equalsIgnoreCase("date")) {
+                            // at this stage we not interested in the date.
+                            continue;
+                        } else {
+                            // each spot that had integration triggered for
+                            JSONObject integrationResponseObject = rootObject
+                                    .getJSONObject(key);
+                            String responseMessage = extractInfoFromIntegrationResponse(integrationResponseObject);
+
+                            // In this example, we just show Toast with the response to user
+                            Toast toast = Toast.makeText(MainActivity.this, responseMessage, Toast.LENGTH_LONG);
+                            toast.setGravity(Gravity.TOP | Gravity.CENTER_VERTICAL, 10, 10);
+                            toast.show();
+
+                        }
+                    }
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+    /**
+     * Extracting data from integration response.
+     * This method is specific to the integration and its response. Hence you need to write it yourself.
+     * This example is specific to parsing response from http://api.fixer.io/
+     *
+     * @param integrationResponseObject - json of the response from 3rd party server.
+     * @return formatted message
+     */
+    private String extractInfoFromIntegrationResponse(JSONObject integrationResponseObject) {
+
+        String baseCurrency = null;
+        String euroExchangeRage = null;
+        String returnMessage = "Data not received or corrupted";
+
+        try {
+            // HTTP GET WEBHOOK
+            String httpGetWebhookResponseString = integrationResponseObject
+                    .getString("httpGetWebhook");
+            Log.d(TAG, "httpGetWebhookResponseString: " + httpGetWebhookResponseString);
+            JSONObject httpGetWebhookResponseObject = new JSONObject(
+                    httpGetWebhookResponseString);
+            JSONObject ratesDictionary = httpGetWebhookResponseObject
+                    .getJSONObject("rates");
+            Log.d(TAG, "ratesDictionary: " + ratesDictionary);
+            baseCurrency = (String) httpGetWebhookResponseObject
+                    .get("base");
+            Log.d(TAG, "baseCurrency: " + baseCurrency);
+            Double ratesEURValue = (Double) ratesDictionary
+                    .get("EUR");
+            Log.d(TAG, "ratesEURValue: " + ratesEURValue);
+            euroExchangeRage = "" + ratesEURValue;
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        if ((baseCurrency != null) && (euroExchangeRage != null)) {
+            returnMessage = "Exchange rate from " + baseCurrency + " to Euro is: " + euroExchangeRage;
+        }
+        return returnMessage;
     }
 
     private void setInRange(final Spot spot) {
         TextView rangeText = (TextView) findViewById(R.id.activity_range_text);
         rangeText.setText(getString(R.string.message_in_range) + " " + spot.name);
+
+        View spotDataButton = findViewById(R.id.activity_spot_data_text);
+        spotDataButton.setVisibility(View.VISIBLE);
+        spotDataButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SpotDataActivity.class);
+                intent.putExtra(Spotz.EXTRA_SPOTZ, spot);
+                startActivity(intent);
+            }
+        });
 
         if (spot.enteredBeacon != null) {
             TextView serialText = (TextView) findViewById(R.id.activity_serial_text);
@@ -153,42 +278,38 @@ public class MainActivity extends Activity {
             serialText.setText(spot.enteredBeacon.serial);
         }
 
-        if (!isInRange) {
+        if (inSpotMap.isEmpty()) {
             TransitionDrawable transition = (TransitionDrawable) findViewById(R.id.wave).getBackground();
             transition.resetTransition();
             transition.startTransition(400);
-
-            View spotDataButton = findViewById(R.id.activity_spot_data_text);
-            spotDataButton.setVisibility(View.VISIBLE);
-            spotDataButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent intent = new Intent(MainActivity.this, SpotDataActivity.class);
-                    intent.putExtra(Spotz.EXTRA_SPOTZ, spot);
-                    startActivity(intent);
-                }
-            });
         }
 
-        isInRange = true;
+        inSpotMap.put(spot.id, spot);
     }
 
-    private void setOutOfRange() {
-        TextView rangeText = (TextView) findViewById(R.id.activity_range_text);
-        rangeText.setText(R.string.message_not_in_range);
+    private void setOutOfRange(final Spot spot) {
+        if (spot != null) {
+            inSpotMap.remove(spot.id);
+        }
 
-        TextView serialText = (TextView) findViewById(R.id.activity_serial_text);
-        serialText.setVisibility(View.GONE);
+        if (inSpotMap.isEmpty()) {
 
-        if (isInRange) {
+            TextView rangeText = (TextView) findViewById(R.id.activity_range_text);
+            rangeText.setText(R.string.message_not_in_range);
+
+            TextView serialText = (TextView) findViewById(R.id.activity_serial_text);
+            serialText.setVisibility(View.GONE);
+
             TransitionDrawable transition = (TransitionDrawable) findViewById(R.id.wave).getBackground();
             transition.resetTransition();
             transition.reverseTransition(400);
 
             findViewById(R.id.activity_spot_data_text).setVisibility(View.INVISIBLE);
-        }
 
-        isInRange = false;
+
+        } else {
+            setInRange((Spot) inSpotMap.values().toArray()[0]);
+        }
     }
 
     @Override
